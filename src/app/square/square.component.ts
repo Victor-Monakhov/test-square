4c8de6da97f78e94bfa79fa9a99638f80aa04b57
Victor Monakhov <Victormonakhov92@gmail.com>
2021-11-05 18:00:45 +0200

    Commit after test

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -6,47 +6,85 @@ import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
   styleUrls: ['./square.component.scss']
 })
 export class SquareComponent implements OnInit {
-
+  container: HTMLElement = document.createElement('div');
   squares: HTMLElement[] = [];
   colors: string[] = [
-    'blue',
-    'red',
+    'brown',
+    'purple',
     'green'
   ];
-  usersSquares:HTMLElement[] = [];
+  clicks: boolean[] = [false, false, false, false, false];
   constructor(private renderer:Renderer2, private el: ElementRef) {
-    for (let i = 0; i < 6; i++){
-      this.squares.push(this.createSquare());
-    }
-    this.el.nativeElement.style.display = 'flex';
-    this.el.nativeElement.style.flexDirection = 'row';
+    this.container.style.display = 'flex';
+    this.container.style.flexDirection = 'row';
+    this.renderer.appendChild(el.nativeElement, this.container);
   }
 
 
   ngOnInit(): void {
     for (let i = 0; i < 6; i++){
-      this.renderer.appendChild(this.el.nativeElement, this.squares[i]);
+      this.squares.push(this.createSquare(`${i}`));
+      this.renderer.appendChild(this.container, this.squares[i]);
     }
+    this.checkBlue();
   }
-  public createSquare(){
+  public createSquare(id: string){
     const square  = document.createElement('div');
-     square.style.backgroundColor = `${this.colors[Math.floor(Math.random() * 3)]}`;
+    square.id = id;
+     square.style.backgroundColor = `${this.colors[Math.floor(Math.random() * this.colors.length)]}`;
      square.style.width = '60px';
      square.style.height = '60px';
      square.style.margin = '50px';
-     square.onclick = this.choice;
+     square.style.border = "5px solid transparent";
+     square.onclick = () =>{
+       this.clicks[+square.id] = !this.clicks[+square.id];
+       square.style.border = (this.clicks[+square.id]) ? "5px solid orange" : "5px solid transparent";
+       console.log(+square.id);
+     }
      return square;
   }
 
-  public choice(){
-    //this.usersSquares.push(square);
+  checkBlue(){
+    let pastId = [];
+    while(true){
+      let id = Math.floor(Math.random() * 6);
+      if(pastId.length === 3) break;
+      if(pastId.find((pId)=>id===pId) === undefined) {
+        pastId.push(id);
+        this.squares[id].style.backgroundColor = 'blue';
+      }
+    }
   }
 
-  public onClick(){
-    this.usersSquares.forEach((square)=>{
-      console.log(square.style.color);
-    });
+  onClick(){
+    let counterTrue = 0;
+    let counterFalse = 0;
+    for(let i = 0; i < this.squares.length; ++i){
+      if(this.clicks[+this.squares[i].id] && this.squares[i].style.backgroundColor === 'blue'){
+        ++counterTrue;
+      }
+      else if(this.clicks[+this.squares[i].id]){
+        this.messageError();
+        return;
+      }
+    }
+    if(counterTrue === 3){
+      this.messageAllRight();
+    }
+    else{
+      this.messageError();
+    }
+  }
+
+  messageError(){
+    const message  = document.createElement('h3');
+    this.renderer.appendChild(this.el.nativeElement,message);
+    message.textContent = 'ERROR';
   }
   
-
+  messageAllRight(){
+    const message  = document.createElement('h3');
+    this.renderer.appendChild(this.el.nativeElement,message);
+    message.textContent = 'All Right';
+  }
 }
